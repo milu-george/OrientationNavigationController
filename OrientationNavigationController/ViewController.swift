4850f532bef9cb2717089af2ad00e7e3bd0f08f7
Milu Ann George <miluann@gmail.com>
2024-01-08 11:24:13 +0530

    Initial Commit

---
+++ OrientationNavigationController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrientationNavigationController
+//
+//  Created by Milu Ann George on 08/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
